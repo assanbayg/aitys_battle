e0c253a839572d0dfef5483f534f2d68578a889e
Gauhar Assanbay <asanbayg@gmail.com>
2023-07-12 10:34:28 +0600

    fix: courtroom scene replies

--- components/aitys/CourtroomScene.jsx
+++ components/aitys/CourtroomScene.jsx
@@ -8,7 +8,7 @@ import Character from "./Character";
 const CourtroomScene = ({ character1, character2, replies }) => {
   const dombraSoundRef = useRef(new Audio("/dombra-sound.mp3"));
   const [currentReplyIndex, setCurrentReplyIndex] = useState(0);
-  const [currentCharacter, setCurrentCharacter] = useState(1);
+  const [currentCharacter, setCurrentCharacter] = useState(0);
 
   const stopSound = () => {
     const dombraSound = dombraSoundRef.current;
@@ -33,35 +33,46 @@ const CourtroomScene = ({ character1, character2, replies }) => {
     if (currentReplyIndex === 0) {
       return;
     }
-    currentCharacter === 1 ? setCurrentCharacter(2) : setCurrentCharacter(1);
+    currentCharacter === 0 ? setCurrentCharacter(1) : setCurrentCharacter(0);
     setCurrentReplyIndex(currentReplyIndex - 1);
   };
 
   const handleNextClick = () => {
+    console.log("CURREnt: ", currentReplyIndex);
     stopSound();
     playSound();
-    if (currentReplyIndex === replies.length - 1) {
+    if (
+      currentReplyIndex === replies.replies.length - 1 &&
+      currentCharacter === 1
+    ) {
       setCurrentReplyIndex(0);
-      setCurrentCharacter(1);
+      setCurrentCharacter(0);
       return;
     }
-    currentCharacter === 1 ? setCurrentCharacter(2) : setCurrentCharacter(1);
-    setCurrentReplyIndex(currentReplyIndex + 1);
+    if (currentCharacter === 0) {
+      setCurrentCharacter(1);
+    } else {
+      setCurrentCharacter(0);
+      setCurrentReplyIndex(currentReplyIndex + 1);
+    }
   };
 
   const transitions = useTransition(currentCharacter, {
     from: {
       opacity: 0,
       transform:
-        currentCharacter === 1 ? "translateX(-100%)" : "translateX(100%)",
+        currentCharacter === 0 ? "translateX(-100%)" : "translateX(100%)",
     },
     enter: { opacity: 1, transform: "translateX(0%)" },
     leave: {
       opacity: 0,
       transform:
-        currentCharacter === 1 ? "translateX(100%)" : "translateX(-100%)",
+        currentCharacter === 0 ? "translateX(100%)" : "translateX(-100%)",
     },
   });
+
+  console.log(currentReplyIndex);
+  console.log(replies.replies[currentReplyIndex]);
 
   return (
     <div className="flex flex-col items-center">
@@ -69,16 +80,17 @@ const CourtroomScene = ({ character1, character2, replies }) => {
       <div className="mb-5 flex">
         {transitions((style) => (
           <animated.div style={style} className="flex items-center gap-x-5">
-            {currentCharacter === 1 ? (
+            {currentCharacter === 0 ? (
               <Character name={character1} image="/character1.png" />
             ) : (
               <Character name={character2} image="/character2.png" />
             )}
             <Reply
               text={
-                currentCharacter === 1
-                  ? replies[currentReplyIndex][character1]
-                  : replies[currentReplyIndex][character2]
+                currentCharacter === 0 ||
+                replies.replies[currentReplyIndex] != "undefined"
+                  ? replies.replies[currentReplyIndex][character1]
+                  : replies.replies[currentReplyIndex][character2]
               }
             />
           </animated.div>
