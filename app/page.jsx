31b8d40da7d5f4fdb27289cdcc198f866bbea5f9
Assanbay Gauhar <asanbayg@gmail.com>
2023-08-01 16:20:55 +0600

    Fix landing"

--- app/page.jsx
+++ app/page.jsx
@@ -4,42 +4,64 @@ import Landing from "@/components/home/landing";
 import Link from "next/link";
 import Divider from "@/components/home/divider";
 import Footer from "@/components/layout/footer";
+import Image from "next/image";
+import About from "@/components/home/about";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   return session !== null ? (
     <>
-      <div
-        className="my-16 h-96 w-auto bg-center bg-no-repeat"
-        style={{
-          backgroundImage: "url('oyu-bg.png')",
-          backgroundSize: "contain",
-        }}
-      >
-        <div className="relative z-10 flex h-full flex-col items-center justify-center space-y-8 px-4 py-16">
-          <h1 className="text-center text-4xl font-extrabold tracking-tight  sm:text-5xl md:text-6xl">
-            GENERATIVE AI MEETS
-            <br /> HISTORY OF KAZAKHSTAN
-          </h1>
-          <button className="mt-8 rounded-2xl bg-[#F08A8A] px-12 py-3 text-3xl text-white">
-            <Link href="/choose-characters">Start Aitys</Link>
-          </button>
-        </div>
-      </div>
-      <Divider />
-      <div className="flex h-auto w-full items-center bg-gradient-to-b from-transparent to-rose-300 sm:h-[70vh]">
+      <div className="bg-gradient">
         <div
-          className="relative my-8 flex h-[60vh] w-full items-center justify-center bg-center bg-no-repeat sm:my-16"
+          className="my-16 h-96 w-auto bg-center bg-no-repeat md:flex"
           style={{
-            backgroundImage: "url('alash.png')",
+            backgroundImage: "url('oyu-bg.png')",
             backgroundSize: "contain",
           }}
         >
-          <h1 className="text-center text-2xl font-bold italic tracking-tight text-white sm:text-4xl md:text-5xl">
-            Сөздің ең ұлысы - тарих
-          </h1>
+          <Image
+            src={"/dombra-big-flip.png"}
+            width={360}
+            height={360}
+            alt="aitys"
+            className="dombra-animated flipped hidden md:block"
+          />
+
+          <div className="relative z-10 flex h-full flex-col items-center justify-center space-y-8 px-4 py-16">
+            <h1 className="text-center text-4xl font-extrabold tracking-tight  sm:text-5xl md:text-6xl">
+              GENERATIVE AI MEETS
+              <br /> HISTORY OF KAZAKHSTAN
+            </h1>
+            <button className="mt-8 rounded-2xl bg-[#F08A8A] px-12 py-3 text-3xl text-white">
+              <Link href="/choose-characters">Start Aitys</Link>
+            </button>
+          </div>
+          <Image
+            src={"/dombra-big.png"}
+            width={360}
+            height={360}
+            alt="aitys"
+            className="dombra-animated hidden md:block"
+          />
         </div>
+        <Divider />
+        <About />
+        <Divider />
+        <div className="flex h-auto w-full items-center  sm:h-[70vh]">
+          <div
+            className="relative my-8 flex h-[60vh] w-full items-center justify-center bg-center bg-no-repeat sm:my-16"
+            style={{
+              backgroundImage: "url('alash.png')",
+              backgroundSize: "contain",
+            }}
+          >
+            <h1 className="text-center text-2xl font-bold italic tracking-tight text-white sm:text-4xl md:text-5xl">
+              Сөздің ең ұлысы - тарих
+            </h1>
+          </div>
+        </div>
+        <Footer />
       </div>
       <Footer />
     </>
